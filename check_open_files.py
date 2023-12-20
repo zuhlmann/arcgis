e482326f8c99335f3f355924a4e205970ea04e23
zuhlmann <uhlmannz@gmail.com>
2023-12-20 16:10:41 -0700

    testing deleting files

--- check_open_files.py
+++
@@ -1,9 +0,0 @@
-# https://stackoverflow.com/questions/6825994/check-if-a-file-is-open-in-python
-
-
-def check_open(fp_csv):
-    try:
-        myfile = open("myfile.csv", "r+") # or "a+", whatever you                    # exit the loop
-    except IOError:
-        print("Could not open file! Please close Excel. Press Enter to retry.")
-        # restart the loop
--- delete_agol.py
+++
@@ -1,8 +0,0 @@
-mcmjac_gis = GIS(username = 'uhlmann@mcmjac.com', password = 'Gebretekle24!')
-print('Connected to {} as {}'.format(mcmjac_gis.properties.portalHostname, mcmjac_gis.users.me.username))
-# # query_str = 'owner:'.format(gis.users.me.username)
-# # my_content = gis.content.search(query = query_str, item_type = 'shapefile', max_items = 15)
-# to get group info use group id from html 'html?id-<HERE IS ID #>'
-krrp_geospatial = Group(mcmjac_gis, 'a6384c0909384a43bfd91f5d9723912b')
-# fc_item.share(groups = 'a6384c0909384a43bfd91f5d9723912b')
-print('ct = {} \\n fc_item {} '.format(idx, fc_item))
